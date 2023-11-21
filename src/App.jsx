74857d1921b29949858d9a610691068c0270bad8
Hugo Petla <hugopetla@gmail.com>
2023-11-21 20:18:43 -0300

    Created the post interaction

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,60 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+// author: { avatar_url: "", name: "", role: "" }
+// publishedAt: Date
+// content: String
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/HugoPetla.png",
+      name: "Hugo Petla",
+      role: "Frontend",
+    },
+    publishedAt: new Date("2023-11-21 17:27:27"),
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um projeto no meu portifa. É um",
+      },
+      {
+        type: "paragraph",
+        content:
+          "projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é",
+      },
+      { type: "paragraph", content: "DoctorCare 🚀 👉 " },
+      { type: "link", content: "jane.design/doctorcare" },
+      { type: "link", content: "#novoprojeto" },
+      { type: "link", content: "#nlw" },
+      { type: "link", content: "#rocketseat" },
+    ],
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/HugoPetla.png",
+      name: "Hugo Petla",
+      role: "Frontend",
+    },
+    publishedAt: new Date("2023-11-01 17:27:27"),
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content: "Acabei de subir mais um projeto no meu portifa. É um",
+      },
+      {
+        type: "paragraph",
+        content:
+          "projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+  },
+];
+
 function App() {
   return (
     <div>
@@ -13,8 +67,16 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
