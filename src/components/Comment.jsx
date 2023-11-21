2cfcb0f7de04e94adeb0bac18c6c676d1a655f01
Hugo Petla <hugopetla@gmail.com>
2023-11-21 10:32:07 -0300

    Created a generic Avatar component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,15 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+
+import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://avatars.githubusercontent.com/u/18637700?v=4" />
+      <Avatar
+        src="https://avatars.githubusercontent.com/u/18637700?v=4"
+        hasBorder={false}
+      />
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
